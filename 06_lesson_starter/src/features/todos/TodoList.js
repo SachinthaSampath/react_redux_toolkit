4c12f8f435eb2827cef34a9d73fb8efe374f37d6
Sachintha Sampath <sachinsampath42@gmail.com>
2024-09-04 01:34:45 +0530

    6: create RTK logic

--- 06_lesson_starter/src/features/todos/TodoList.js
+++ 06_lesson_starter/src/features/todos/TodoList.js
@@ -1,44 +1,69 @@
 // add imports
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { useAddTodoMutation, useDeleteTodoMutation, useGetTodosQuery, useUpdateTodoMutation } from '../api/apiSlice';
 
 const TodoList = () => {
-    const [newTodo, setNewTodo] = useState('')
+  const [newTodo, setNewTodo] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //addTodo
-        setNewTodo('')
-    }
+  const { data: todos, error, isLoading, isError, isSuccess } = useGetTodosQuery();
+  const [addTodo] = useAddTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
 
-    const newItemSection =
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="new-todo">Enter a new todo item</label>
-            <div className="new-todo">
-                <input
-                    type="text"
-                    id="new-todo"
-                    value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
-                    placeholder="Enter new todo"
-                />
-            </div>
-            <button className="submit">
-                <FontAwesomeIcon icon={faUpload} />
-            </button>
-        </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({
+      id: `${100 + todos.length + 1}`,
+      userId: 1,
+      title: newTodo,
+      completed: false,
+    });
+    setNewTodo('');
+  };
 
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input type="text" id="new-todo" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder="Enter new todo" />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
 
-    let content;
-    // Define conditional content
+  let content;
+  // Define conditional content
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (isSuccess) {
+    content = todos.map((todo) => {
+      //JSON.stringify(todos)
+      return (
+        <article key={todo.id}>
+          <div className="todo">
+            <input type="checkbox" checked={todo.completed} id={todo.id} onChange={() => updateTodo({ ...todo, completed: !todo.completed })} />
+            <label htmlFor={todo.id}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <div>Error: {error}</div>;
+  }
 
-    return (
-        <main>
-            <h1>Todo List</h1>
-            {newItemSection}
-            {content}
-        </main>
-    )
-}
-export default TodoList
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </main>
+  );
+};
+export default TodoList;
